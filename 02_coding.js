05184efd2d5bb4b13ba55d906cfcb6e42f377aba
Adrian Ferreres <ardiadrianadri@gmail.com>
2017-07-21 17:27:20 +0200

    Solution 03

--- 02_coding.js
+++ 02_coding.js
@@ -2,14 +2,14 @@ let observable = (observer) => {
     observer.next("Hello world");
 }
 
-let operator = (observable) => {
+let operator = (observable, map) => {
     return (observer) => {
         return observable({
-            next: (x) => {observer.next(x + '!!');} 
+            next: (x) => {observer.next(map(x));} 
         })
     }
 }
 
-operator(observable)({
+operator(observable, (x) => x + '!!')({
     next: (value) => { console.log (value); }
 });
